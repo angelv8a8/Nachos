5d7ec2c569132eef4d2512136aed1abacd89e0af
Angel Ochoa <angelv8a8@hotmail.com>
2014-01-27 23:00:34 -0600

    Gravedad y disparos
    Agregue disparos y gravedad a el nivel de tierra

--- NachosAgony/src/com/ccvii/nachos/GameBoardTierra.java
+++ NachosAgony/src/com/ccvii/nachos/GameBoardTierra.java
@@ -12,7 +12,6 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.View;
@@ -27,6 +26,15 @@ public class GameBoardTierra extends View{
 	private List<Disparo> disparos = new ArrayList<Disparo>();
 	public int x = 900;
 	public int y = 550;
+	
+	public int moveX = 1;
+	public int moveY = 1;
+	
+	
+	public boolean movingRight = false;
+	public boolean movingLeft = false;
+	public boolean movingUp = false;
+	public boolean movingDown = false;
 	
 	private Bitmap bmp = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_launcher);
 	
@@ -76,9 +84,40 @@ public class GameBoardTierra extends View{
 		p.setStrokeWidth(5);
 		
 		
+		
+		
+		if(movingLeft)
+			x-= 5;
+		else if (movingRight)
+			x+= 5;
+		
+		if(y < 600){
+			if(!movingUp)
+			 y+= 10;
+		}
+		else if (y >= 600 )
+		{
+			movingDown = false;
+			
+		}
+		
+		if( movingUp)
+		{ 
+			if(y > 450)
+				y-=10;
+			else {
+				movingUp=false;
+				movingDown = true;
+			}
+		}
+		else if (y <= 450)
+		{
+			movingUp = false;
+			movingDown = true;
+		}
+		
+		
 		canvas.drawBitmap(bmp,x,y,p);
-		
-		
 		
 		
 		if(debug)
@@ -91,20 +130,18 @@ public class GameBoardTierra extends View{
 		{
 			canvas.drawCircle(disparos.get(e).x, disparos.get(e).y, 5,p);
 			
-			disparos.get(e).x -= disparos.get(e).moveX;
-			disparos.get(e).y += disparos.get(e).moveY;
+			disparos.get(e).x += disparos.get(e).moveX;
+			disparos.get(e).y -= disparos.get(e).moveY;
 		}
-		
-		
 	}
 	
 	@Override
 	  public boolean onTouchEvent(MotionEvent event) {
 	    
-	    
 	 // get pointer index from the event object
 	    int pointerIndex = event.getActionIndex();
 
+	    
 	    // get pointer ID
 	    int pointerId = event.getPointerId(pointerIndex);
 
@@ -112,35 +149,73 @@ public class GameBoardTierra extends View{
 	    int maskedAction = event.getActionMasked();
 
 	    switch (maskedAction) {
-
 	    case MotionEvent.ACTION_DOWN:
-	    case MotionEvent.ACTION_POINTER_DOWN: {
-
-	    	Log.i(GameBoardTierra.class.toString() , x + " "+ y);
-	    }
-	    case MotionEvent.ACTION_MOVE: { // a pointer was moved
+	    case MotionEvent.ACTION_POINTER_DOWN:
+	        
 	    	
+	    	if( event.getPointerCount()< 2) {
+	  	      
+		     if(event.getX(0)  > this.getWidth() -  250) 
+			    	this.AgregarDisparo((int)event.getX(0) ,(int)event.getX(0) );
+	    	}else
+	    	{
+	    		if(event.getX(1)  > this.getWidth() -  250) 
+			    	this.AgregarDisparo((int)event.getX(1) ,(int)event.getX(1) );
+	    	}
+		     break;
+		     
+	    case MotionEvent.ACTION_MOVE: 
+	    case MotionEvent.ACTION_HOVER_ENTER:
 	    	
+	    	int tX =  (int)event.getX();
+	        int tY = (int)event.getY();
 	    	
-	     if(event.getX() < 200)
-	    	 x -= 5;
-	     else
-	    	 x += 5;
-	      
-	     
-	      break;
-	    }
+		     if(tX < 200)
+		    	 movingLeft = true;
+		     else if(tX< 400)
+		    	 movingRight = true;
+		     
+		     if(tX < 400)
+		     { 
+		    	 if(tY < 550)
+		    		 movingUp = true;
+		     }
+		
+		     break;
 	    case MotionEvent.ACTION_UP:
 	    case MotionEvent.ACTION_POINTER_UP:
+	    	
+	    	if(!movingUp && !movingDown)
+	    	{
+		    	movingLeft = false;
+		    	movingRight = false;
+	    	}
+	    	break;
+	    
 	    case MotionEvent.ACTION_CANCEL: {
-	   
 	      break;
 	    }
+	   
+	    
 	    }
 
+	    
 	    //CalculateMidBallPosition();
 	    return true;
 	  }
+
+	private void AgregarDisparo(int aX, int aY)
+	{
+		int moveX = 10;
+
+		int difY = (this.y-aY);
+		int difX = aX-this.x;
+		
+		int moveY = moveX /((int)((difX)/ ((difY))));
+		
+		Disparo aux = new Disparo(this.x,this.y, moveX,moveY);
+		this.disparos.add(aux);
+	}
 	
 	
 	
